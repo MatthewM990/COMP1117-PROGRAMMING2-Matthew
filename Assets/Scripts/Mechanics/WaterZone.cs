c59cfb830812f54903fb01a037926f3246ba0285
MatthewM990 <pennywenny411@gmail.com>
2026-02-04 11:37:13 -0500

    Created Speed Boost Zone

--- Assets/Scripts/Mechanics/WaterZone.cs
+++ Assets/Scripts/Mechanics/WaterZone.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class WaterZone : Zone
 {
-    [Range(0f, 1f)]
+    [Range(0f, 5f)]
     [SerializeField] private float speedModifier = 0.5f;
 
     protected override void ApplyZoneEffect(Player player)
