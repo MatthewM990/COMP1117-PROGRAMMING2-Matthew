0dfd81ecb2d2557c77fa327cc6aea27e381dd68b
MatthewM990 <pennywenny411@gmail.com>
2026-02-11 11:36:43 -0500

    Added 2 new interactables

---
+++ Assets/Scripts/NPC/NPCLogic.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class NPCLogic : MonoBehaviour, IInteractable
+{
+    [SerializeField] private GameObject speechBubble;
+    public void Interact()
+    {
+        // Safety check
+        if(speechBubble == null)
+        {
+            // If the speech bubble doesn't exist return immediately and do nothing.
+            return;
+        }
+
+        // speech bubble DOES exist!
+        bool isCurrentlyActive = speechBubble.activeSelf;
+
+        speechBubble.SetActive(!isCurrentlyActive);
+
+        Debug.Log("NPC interaction toggled");
+    }
+}
